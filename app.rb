872f17c90bfde01f973680748e62842a92d077e2
luisarita <github@luisarita.com>
2017-07-19 10:31:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
+    post '/team' do
+        @team = {}
+        @team[:name] = params[:name]
+        @team[:coach] = params[:coach]
+        @team[:pg] = params[:pg]
+        @team[:sg] = params[:sg]
+        @team[:pf] = params[:pf]
+        @team[:sf] = params[:sf]
+        @team[:c] = params[:c]
+        erb :team
+    #    redirect '/team'
+    end
 
+    #get '/team' do
+    #    erb :team
+    #end
 end
